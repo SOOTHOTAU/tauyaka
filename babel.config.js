959bbbfb291a3a95b0d478dd789e21969609493a
SOOTHOTAU <163233761+SOOTHOTAU@users.noreply.github.com>
2025-10-28 23:01:54 +0200

    Format babel.config.js and add reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-﻿module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // …any other plugins you use
+      'react-native-reanimated/plugin'
+    ]
   };
 };
