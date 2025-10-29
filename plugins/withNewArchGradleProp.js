249dd872adc2555555d9eb791a96e98c5b416f54
SOOTHOTAU <163233761+SOOTHOTAU@users.noreply.github.com>
2025-10-29 20:19:09 +0200

    Add newArchEnabled property to Gradle config

--- plugins/withNewArchGradleProp.js
+++ plugins/withNewArchGradleProp.js
@@ -1,1 +1,14 @@
 
+// plugins/withNewArchGradleProp.js
+const { withGradleProperties } = require("@expo/config-plugins");
+
+module.exports = function withNewArchGradleProp(config) {
+  return withGradleProperties(config, (cfg) => {
+    const props = cfg.modResults.filter(
+      (p) => p.type !== "property" || p.key !== "newArchEnabled"
+    );
+    props.push({ type: "property", key: "newArchEnabled", value: "true" });
+    cfg.modResults = props;
+    return cfg;
+  });
+};
