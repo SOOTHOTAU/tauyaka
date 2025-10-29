c015e0ea060df169b37e5859ca53a9eb9129ddd0
Sootho Tau <sootho.tau@gmail.com>
2025-10-29 08:01:38 +0000

    fix: pin reanimated 3.8.1, disable new architecture

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // …any other plugins you use
-      'react-native-reanimated/plugin' // keep this last
-    ]
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
