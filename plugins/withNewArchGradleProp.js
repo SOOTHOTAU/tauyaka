6886735e0bd99bfd06f7e934077441c45b040e2a
Sootho Tau <sootho.tau@gmail.com>
2025-10-30 08:28:44 +0000

    build: add local plugin to force newArchEnabled (fallback)

--- plugins/withNewArchGradleProp.js
+++ plugins/withNewArchGradleProp.js
@@ -1,14 +1,15 @@
-
-// plugins/withNewArchGradleProp.js
-const { withGradleProperties } = require("@expo/config-plugins");
+﻿const { withGradleProperties } = require("@expo/config-plugins");
 
 module.exports = function withNewArchGradleProp(config) {
-  return withGradleProperties(config, (cfg) => {
-    const props = cfg.modResults.filter(
-      (p) => p.type !== "property" || p.key !== "newArchEnabled"
-    );
-    props.push({ type: "property", key: "newArchEnabled", value: "true" });
-    cfg.modResults = props;
-    return cfg;
+  return withGradleProperties(config, async ({ modResults }) => {
+    modResults = modResults || [];
+    const key = "newArchEnabled";
+    const existing = modResults.find((p) => p.type === "property" && p.key === key);
+    if (existing) {
+      existing.value = "true";
+    } else {
+      modResults.push({ type: "property", key, value: "true" });
+    }
+    return { modResults };
   });
 };
